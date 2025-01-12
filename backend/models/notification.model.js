ebdff606aa01fce02e5938492ace88d6b3f44222
vjkumar0208 <uniquevijay0208@gmail.com>
2025-01-12 16:32:26 +0530

    1-user routes and controllers added

---
+++ backend/models/notification.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const NotificationSchema=new mongoose.Schema({
+    from:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    to:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:true
+    },
+    type:{
+        type:String,
+        required:true,
+        enum:["follow","like"]
+    }
+    ,
+    read:{
+        type:Boolean,
+        default:false
+    },
+},{timestamps:true});
+
+const Notification=mongoose.model("Notification",NotificationSchema);
+
+export default Notification;
