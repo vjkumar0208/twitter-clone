66939e30002ca5dc234c4234470cef3bb3dba807
vjkumar0208 <uniquevijay0208@gmail.com>
2025-01-26 19:23:56 +0530

    updating some minor errors

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import LoadingSpinner from "../../components/Common/LoadingSpinner";
+import LoadingSpinner from "../../components/common/LoadingSpinner.jsx";
 import {toast} from "react-hot-toast"
 import { useQuery,useMutation, useQueryClient } from "@tanstack/react-query";
 import { IoSettingsOutline } from "react-icons/io5";
